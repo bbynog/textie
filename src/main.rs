97d30e2ef5b5cf07b23bb5aff6cd4d4e651ff251
DaviRolim <davirolim94@gmail.com>
2024-01-05 07:51:17 -0300

    refactoring and adding a README file

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,85 @@
+use chrono::prelude::*;
+use std::error;
 use std::fs::File;
 use std::io::prelude::*;
-use chrono::prelude::*;
 use std::process::Command;
-use std::error;
 
 const DEFAULT_OUTPUT_DIR: &'static str = include_str!("./constants.txt");
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
+fn get_output_dir() -> String {
+    let mut output_dir = String::new();
+
+    println!(
+        "Where would you like to save the file? (DEFAULT: {})",
+        DEFAULT_OUTPUT_DIR
+    );
+    std::io::stdin()
+        .read_line(&mut output_dir)
+        .expect("Failed to read input");
+
+    let output_dir = output_dir.trim();
+
+    if output_dir.is_empty() {
+        String::from(DEFAULT_OUTPUT_DIR)
+    } else {
+        update_output_dir(output_dir).expect("Failed to update output dir");
+        return output_dir.to_string();
+    }
+}
+
+fn get_file_name(default_file_name: &str) -> String {
+    let mut file_name = String::new();
+
+    println!(
+        "What's the name of the txt file? (DEFAULT: {})",
+        default_file_name
+    );
+    std::io::stdin()
+        .read_line(&mut file_name)
+        .expect("Failed to read input");
+
+    let file_name = file_name.trim();
+
+    if file_name.is_empty() {
+        default_file_name.to_string()
+    } else {
+        file_name.to_string()
+    }
+}
+
+fn create_and_write_file(output_dir: &str, file_name: &str, content: &str) -> Result<()> {
+    let current_time_formatted = Local::now().format("%a - %d %b %Y - %T");
+    let txt_content = format!("{} {}\n", content, current_time_formatted);
+
+    let mut text_file =
+        File::create(format!("{}/{}.txt", output_dir, file_name)).expect("Failed to create file");
+
+    text_file.write_all(txt_content.as_bytes())?;
+
+    Ok(())
+}
+
+fn open_file(output_dir: &str, file_name: &str) -> Result<()> {
+    Command::new("open")
+        .arg(format!("{}/{}.txt", output_dir, file_name))
+        .spawn()?;
+
+    Ok(())
+}
 fn update_output_dir(output_dir: &str) -> Result<()> {
     std::fs::write("./src/constants.txt", output_dir)?;
     Ok(())
 }
 
 fn main() -> Result<()> {
-    let mut file_name = String::new();
-    
-    let current_time = Local::now();
+    let output_dir = get_output_dir();
+    let default_file_name = Local::now().format("%d-%m-%Y_%H:%M:%S").to_string();
+    let file_name = get_file_name(&default_file_name);
 
-    let default_file_name = current_time.format("%d-%m-%Y_%H:%M:%S").to_string();
-
-    let mut output_dir = String::new();
-
-    println!("Where you'd like to save the file? (DEFAULT: {DEFAULT_OUTPUT_DIR})");
-    std::io::stdin().read_line(&mut output_dir).unwrap();
-
-    let mut output_dir = output_dir.trim();
-
-    if output_dir.is_empty() {
-        output_dir = DEFAULT_OUTPUT_DIR;
-    } else {
-        update_output_dir(output_dir)?;
-    }
-
-    println!("What's the name of the txt file? (DEFAULT: {default_file_name})");
-    std::io::stdin().read_line(&mut file_name).unwrap();
-
-    let mut file_name = file_name.trim();
-
-    let current_time_formatted = current_time.format("%a - %d %b %Y - %T");
-
-    let mut txt_content = format!("{current_time_formatted} \n");
-
-    if file_name.is_empty() {
-        file_name = &default_file_name;
-    } else {
-        txt_content = format!("{file_name} - {current_time_formatted}\n");
-    }
-
-    let mut text_file = File::create(format!("{output_dir}/{file_name}.txt")).unwrap();
-    
-    text_file.write_all(txt_content.as_bytes()).unwrap();
-
-    Command::new("open")
-            .arg(format!("{output_dir}/{file_name}.txt"))
-            .spawn()
-            .unwrap();
+    create_and_write_file(&output_dir, &file_name, "")?;
+    open_file(&output_dir, &file_name)?;
 
     Ok(())
 }
